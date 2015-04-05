c56ec1a3814cbff0a2fb8c35648191574ee975eb
Kuan-Wei Kuo <k.kuanwei@gmail.com>
2015-04-05 20:11:15 +0800

    update notification finish.

--- app/src/main/java/com/kuo/gold3munite/StatisicsFragment.java
+++ app/src/main/java/com/kuo/gold3munite/StatisicsFragment.java
@@ -1,8 +1,11 @@
 package com.kuo.gold3munite;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -27,6 +30,7 @@ public class StatisicsFragment extends Fragment {
         notificationButton = (Button) view.findViewById(R.id.notificationButton);
 
         notificationButton.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onClick(View view) {
                 RemoteViews contentViews = new RemoteViews(getActivity().getPackageName(), R.layout.status_notification_item);
@@ -38,16 +42,13 @@ public class StatisicsFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-                        getActivity()).setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("My notification")
-                        .setTicker("new message");
-                mBuilder.setAutoCancel(true);
-                mBuilder.setContentIntent(pendingIntent);
-                mBuilder.setContent(contentViews);
-                mBuilder.setAutoCancel(true);
+                Notification notification = new Notification();
+                notification.bigContentView = contentViews;
+                notification.icon=R.mipmap.ic_launcher;
+                notification.defaults=Notification.DEFAULT_SOUND;
+                notification.setLatestEventInfo(getActivity(),"黃金三分鐘","趕快記錄吧！", pendingIntent );
                 NotificationManager notificationManager=(NotificationManager)getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
-                notificationManager.notify(0, mBuilder.build());
+                notificationManager.notify(0, notification);
             }
         });
 
